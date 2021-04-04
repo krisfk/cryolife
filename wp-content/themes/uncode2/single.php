a15721714b25e107b4e559f027ce3fcef83e2700
kaiyu li <krisfk@gmail.com>
2021-04-05 06:11:39 +0800

    .

--- wp-content/themes/uncode2/single.php
+++ wp-content/themes/uncode2/single.php
@@ -680,7 +680,7 @@ while (have_posts()):
 
 	/** Display post html **/
 	the_post_thumbnail( 'full' );
-echo 1111;
+// echo 1111;
 	echo 	'<article id="post-'. get_the_ID().'" class="'.implode(' ', get_post_class('page-body' . $bg_color)) .'">
           <div class="post-wrapper">
           	<div class="post-body">';
