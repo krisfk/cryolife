29b0dbc581fb41dd24018fd70ea18474baedb770
kaiyu li <krisfk@gmail.com>
2021-05-14 04:19:53 +0800

    .

--- wp-content/themes/uncode2/single.php
+++ wp-content/themes/uncode2/single.php
@@ -689,7 +689,7 @@ while (have_posts()):
 echo '<div class="single-post-feature-img-top">';
 if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned to it.
 	
-	the_post_thumbnail( 'full' );
+	// the_post_thumbnail( 'full' );
 	}
 	echo '</div>';
 	?>
