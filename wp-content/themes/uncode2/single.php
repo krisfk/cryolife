11613c7024a7f20e966f3e3d55254aeeccc24a10
kaiyu li <krisfk@gmail.com>
2021-05-18 18:18:59 +0800

    .

--- wp-content/themes/uncode2/single.php
+++ wp-content/themes/uncode2/single.php
@@ -753,11 +753,14 @@ if(get_field('slideshow_on_the_top'))
 
 <script type="text/javascript">
 $(function() {
-    $('.single-post-feature-img-top img').remove()
-    $('.single-post-feature-img-top').append($('.post-media'))
-    $('.tmb .t-entry-visual').eq(0).css({
-        'margin': '0 auto'
-    })
+    setTimeout(() => {
+        $('.single-post-feature-img-top img').remove()
+        $('.single-post-feature-img-top').append($('.post-media'))
+        $('.tmb .t-entry-visual').eq(0).css({
+            'margin': '0 auto'
+        })
+    }, 2000);
+
 
 })
 </script>
