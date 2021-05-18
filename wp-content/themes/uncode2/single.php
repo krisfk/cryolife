4a7fbab8467eab24635c190e9406858ff4a2f32d
kaiyu li <krisfk@gmail.com>
2021-05-18 18:20:04 +0800

    .

--- wp-content/themes/uncode2/single.php
+++ wp-content/themes/uncode2/single.php
@@ -752,17 +752,17 @@ if(get_field('slideshow_on_the_top'))
 	?>
 
 <script type="text/javascript">
-$(function() {
-    // setTimeout(() => {
-    //     $('.single-post-feature-img-top img').remove()
-    //     $('.single-post-feature-img-top').append($('.post-media'))
-    //     $('.tmb .t-entry-visual').eq(0).css({
-    //         'margin': '0 auto'
-    //     })
-    // }, 2000);
+// $(function() {
+// setTimeout(() => {
+//     $('.single-post-feature-img-top img').remove()
+//     $('.single-post-feature-img-top').append($('.post-media'))
+//     $('.tmb .t-entry-visual').eq(0).css({
+//         'margin': '0 auto'
+//     })
+// }, 2000);
 
 
-})
+// })
 </script>
 <?php
 }
