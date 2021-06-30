f19ee2d6821e03ed7c2557a6f1870d80a4d3d78f
kaiyu li <krisfk@gmail.com>
2021-07-01 04:39:28 +0800

    .

--- wp-content/themes/uncode2/header.php
+++ wp-content/themes/uncode2/header.php
@@ -46,6 +46,8 @@ if (is_admin_bar_showing()) {
     <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script> -->
 
     <?php wp_head(); ?>
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri().'/library/css/style-custom.css' ?>"
+        type="text/css" media="screen" charset="utf-8" />
 
     <link rel="stylesheet" href="<?php echo get_template_directory_uri().'/library/css/prettyPhoto.css' ?>"
         type="text/css" media="screen" charset="utf-8" />
