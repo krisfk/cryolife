a09fdace983de59070a8c694584b56e60040b54f
kaiyu li <krisfk@gmail.com>
2021-07-01 06:34:21 +0800

    .

--- wp-content/themes/uncode2/partials/menus.php
+++ wp-content/themes/uncode2/partials/menus.php
@@ -570,7 +570,6 @@ if (!class_exists('unmenu')) {
 
 						$currentLang = qtrans_getLanguage();
 						echo $currentLang;
-						echo 1234567;
 						$search_inner .= 	'<a class="login-btn" href="https://cryolifehk.force.com/customer/s/login" target="_blank">Login</a>';
 						$search_inner .= 	'<a class="register-now-btn" href="https://cryolifehk.formtitan.com/ftd8e33f871615871999320_copy" target="_blank">Register</a>';
 
